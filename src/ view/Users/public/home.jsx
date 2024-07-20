06ce560fdca9a4c6371aa4504a5bc15b27a04c10
alanchrissantony <alanchrissantony@gmail.com>
2024-07-20 22:23:42 +0530

    frontend deploy

---
+++ src/ view/Users/public/home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Departments from '../../../Components/Users/Departments/departments'
+import RowPost from '../../../Components/Users/RowPosts/rowPost';
+
+
+
+
+function Home() {
+
+
+  return (
+      <div>
+          <Departments/>
+          <RowPost/>
+      </div>
+  )
+}
+
+export default Home;
